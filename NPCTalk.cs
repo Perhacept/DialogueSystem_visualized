06a8c9c925cc684ec8c1403536731e0b8cc71fda
Perhacept <31383521+Perhacept@users.noreply.github.com>
2021-04-27 18:48:16 +0800

    Update NPCTalk.cs

--- NPCTalk.cs
+++ NPCTalk.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +9,17 @@ using Subtegral.DialogueSystem.DataContainers;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Components;
 
+
 public class NPCTalk : MonoBehaviour, kejiaohu
 {
     [SerializeField] private DialogueContainer dialogue;
     [SerializeField] private TextMeshProUGUI dialogueText_onUI;
     public LocalizedString mylocalizedstring;
     LocalizedString mylocalizedButtonstring;
-    [SerializeField] private Button choicePrefab;
+    [SerializeField] private GameObject choicePrefab;
     [SerializeField] private Transform buttonContainer;
     public string playerName = "Perh";
+    public GameObject qipao;
 
     PlayerInputActions _playerInputController;
 
@@ -50,105 +52,74 @@ public class NPCTalk : MonoBehaviour, kejiaohu
 
     protected virtual void OpenAdialogue()
     {
-        _playerInputController.RealGamePlay.Enable();
-
+        _playerInputController.DialoguePlay.Enable();
         GameManager.instance._playerState = GameManager.PlayerState.inDialogue;
+        GameManager.instance.playerIns.GetComponent<Animator>().SetBool("onGround", true) ;//todo
+        qipao.SetActive(true);
 
         var narrativeData = dialogue.NodeLinks.First(); //Entrypoint node
-        ProceedToNarrative(narrativeData.TargetNodeGuid);
+        //ProceedToNarrative(narrativeData.TargetNodeGuid);
+        StartCoroutine(ProceedTxtNButtons(narrativeData.TargetNodeGuid));
     }
     protected virtual void CloseAdialogue() {
-        _playerInputController.RealGamePlay.Disable();
+        _playerInputController.DialoguePlay.Disable();
         GameManager.instance._playerState = GameManager.PlayerState.idling;
+        qipao.SetActive(false);
     }
 
-    private void ProceedToNarrative(string narrativeDataGUID)
-    {
-        //get txt
-        string textkey = getTxtStr(narrativeDataGUID);
+    IEnumerator ProceedTxtNButtons(string narrativeDataGUID) {
+        //if (!dialogue.DialogueNodeData.Exists(x => x.NodeGUID == narrativeDataGUID)) {
+           //Debug.Log("done");
+           // CloseAdialogue();
+           // yield return null;
+       // }
+        //else 
+            {
+            string textkey = getTxtStr(narrativeDataGUID);
 
-        mylocalizedstring = new LocalizedString { TableReference = "DialogueTable", TableEntryReference = textkey };
-        Debug.Log(textkey);
-        //mylocalizedstring.SetReference("DialogueTable", textkey);
-        //= new LocalizedString { TableReference = "My String Table Collection", TableEntryReference = "My Text 1" };
-        //mylocalizedstring.Arguments.Add(this.gameObject);
+            mylocalizedstring = new LocalizedString { TableReference = "DialogueTable", TableEntryReference = textkey };
+            //mylocalizedstring.Arguments.Add(this.gameObject);
+            yield return mylocalizedstring.GetLocalizedString();
+            //if (mylocalizedstring.GetLocalizedString().IsDone)
+            //{
+            dialogueText_onUI.text = mylocalizedstring.GetLocalizedString().Result;
+            //}       
+            var choices = dialogue.NodeLinks.Where(x => x.BaseNodeGUID == narrativeDataGUID);//前节点是narrativeDataGUID的data 基础的链接结构//
 
-        var choices = dialogue.NodeLinks.Where(x => x.BaseNodeGUID == narrativeDataGUID);//前节点是narrativeDataGUID的data 基础的链接结构
-        //dialogueText_onUI.text = mylocalizedstring.GetLocalizedString().Result;
-        dialogueText_onUI.text = mylocalizedstring.GetLocalizedString().Result;
-        
-        //dialogueText_onUI.text = ProcessProperties(text);
-
-        //refresh Button
-        //des
-        var buttons = buttonContainer.GetComponentsInChildren<Button>();
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            Destroy(buttons[i].gameObject);
-        }
-        //ini
-        foreach (var choice in choices)
-        {
-            var button = Instantiate(choicePrefab, buttonContainer);
-            //button.GetComponentInChildren<Text>().text = ProcessProperties(choice.PortName);
-            //mylocalizedButtonstring.StringReference.SetReference("DialogueButtonTable", choice.PortName);
-            mylocalizedButtonstring = new LocalizedString { TableReference = "DialogueButtonTable", TableEntryReference = choice.PortName };
-
-            StartCoroutine(modifyLocaleStr(mylocalizedButtonstring.GetLocalizedString(),button));
-            //if (!mylocalizedButtonstring.GetLocalizedString().IsDone)
-                //await mylocalizedButtonstring.GetLocalizedString();
-
-
-            /*
-
-            Debug.Log(mylocalizedButtonstring.CurrentLoadingOperation.ToString());
-            //button.GetComponentInChildren<Text>().text = mylocalizedButtonstring.GetLocalizedString().Result;
-            button.GetComponentInChildren<Text>().text = mylocalizedButtonstring.GetLocalizedString().Result;
-            button.onClick.AddListener(() => ProceedToNarrative(choice.TargetNodeGuid));//事件监听
-
-            */
-        }
-        buttons[buttons.Length - 1].Select();
-        //buttons[0]
-        //started buttonToSet.Select();
-    }
-    private string ProcessProperties(string text)
-    {        
-        foreach (var exposedProperty in dialogue.ExposedProperties)
-        {
-            text = text.Replace($"[{exposedProperty.PropertyName}]", exposedProperty.PropertyValue);           
-        }
-        //or string.Replace(propertyName,runtimeValue);
-        //但因为本地化 用不到
-        return text;
-    }
-
-    string getTxtStr(string _narrativeDataGUID) {
-        /*if (GameManager.instance._currentLanguage == GameManager.WhichLanguage.Chinese)
-        {
-            return dialogue.DialogueNodeData.Find(x => x.NodeGUID == _narrativeDataGUID).DialogueTextCHS;
-        }
-        else if (GameManager.instance._currentLanguage == GameManager.WhichLanguage.English)
-        {
-            return dialogue.DialogueNodeData.Find(x => x.NodeGUID == _narrativeDataGUID).DialogueTextEN;
-        }
-        else
-        {
-            return dialogue.DialogueNodeData.Find(x => x.NodeGUID == _narrativeDataGUID).DialogueTextEN;
-        }*/
-        return dialogue.DialogueNodeData.Find(x => x.NodeGUID == _narrativeDataGUID).DialogueTextEN;
-    }
-
-    private IEnumerator modifyLocaleStr(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<string> temp,Button _buttons)
-    {
-        yield return temp;
-
-        if (temp.IsDone)
-        {
-            Debug.Log(temp.Result);
+            var buttons = buttonContainer.GetComponentsInChildren<Button>();
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                Destroy(buttons[i].gameObject);//warning: Destroy是异步的
+            }
+            yield return null;//至关重要//todo
+            //for (int i=0;i<buttonTextKeys.Count();i++) {}
+            foreach (var choice in choices)
+            {
+                var button = Instantiate(choicePrefab, buttonContainer);
+                mylocalizedButtonstring = new LocalizedString { TableReference = "DialogueButtonTable", TableEntryReference = choice.PortName };
+                yield return mylocalizedButtonstring.GetLocalizedString();
+                button.GetComponentInChildren<TMP_Text>().text = mylocalizedButtonstring.GetLocalizedString().Result;
+                button.GetComponent<Button>().onClick.AddListener(() => {
+                    //if (choice.TargetNodeGuid != "")
+                    {
+                        StartCoroutine(ProceedTxtNButtons(choice.TargetNodeGuid));//这里可以增加特殊条件
+                    }
+                    //else {
+                        //CloseAdialogue();
+                    //}
+                });//事件监听
+            }
+            if (buttonContainer.childCount != 0)
+            {
+                //Debug.Log(buttonContainer.childCount);
+                buttonContainer.GetChild(buttonContainer.childCount - 1).GetComponent<Button>().Select();
+            }
+            else {
+                //Debug.Log("tr2");
+                //yield return null;
+                CloseAdialogue();
+            }
+            yield return null;
         }
     }
-
-
-
 }
